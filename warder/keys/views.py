948f513bdd25121c37dde523a1674d8c2c16037e
Daniel Sharp <dsharpc@gmail.com>
2021-12-07 23:21:05 +0000

    Added login requirement for viewing page

--- warder/keys/views.py
+++ warder/keys/views.py
@@ -3,7 +3,8 @@ from .models import Credential
 
 # Create your views here.
 def home(request):
-
+    if not request.user.is_authenticated:
+        return render(request, 'keys/login_needed.html')
     creds = Credential.objects.order_by('site')
 
     return render(request, 'keys/home.html', {'creds': creds})
